fc0a3cd5561d6bb2261389e76482e87f060a1b20
23BCS10652-KRG3A <23BCS10652@cuchd.in>
2025-12-15 11:21:14 +0530

    Create q1.sql

---
+++ session3/q1.sql
@@ -0,0 +1,17 @@
+SELECT 
+    p.product_name,
+    SUM(o.unit) AS total_units
+FROM 
+    Products p
+JOIN 
+    Orders o
+ON 
+    p.product_id = o.product_id
+WHERE 
+    o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY 
+    p.product_name
+HAVING 
+    SUM(o.unit) >= 100
+ORDER BY 
+    total_units DESC;
